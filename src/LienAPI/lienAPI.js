6a75320f9770ab8869ed711980382b601175a45b
paulineRbln <problin@ensc.fr>
2025-04-13 12:52:52 +0200

    liens pour API et notice installation

--- src/LienAPI/lienAPI.js
+++ src/LienAPI/lienAPI.js
@@ -1,7 +1,8 @@
 export function lienAPIMachine(){
     return lien_localHost
+    //return lien_Ordinateur_Perso
 }
 
 
 const lien_localHost = "http://localhost:5222/api";
-//const lien_Ordinateur_Perso = "";
+//const lien_Ordinateur_Perso = "http://192.168.X.XXX:5222/api";
